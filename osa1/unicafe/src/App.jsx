4b989e9e5e754f177cdf778f780a5acc833766ef
Sanna Martikainen <martikainen.sanna@gmail.com>
2026-04-22 22:06:00 +0300

    Exercises 1.8 and 1.9

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -3,19 +3,41 @@ import { useState } from 'react'
 
 const Statistics = (props) => {
 
+    const getSum = () => {
+      return props.good + props.neutral + props.bad
+    }
+
+    const getAverage = () => {
+      return (props.good*1 + props.neutral*0 + props.bad*(-1)) / 3
+    }
+
+    const getPositive = () => {
+       return (props.good / getSum(props)) *100 + ' %'
+    }
+
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h1>Statistics</h1>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
+      <p>all {getSum()} </p>
+      <p>average {getAverage()} </p>
+      <p>positive {getPositive()} </p>
     </div>
   )
 }
 
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -32,21 +54,6 @@ const App = () => {
       setBad(bad + 1)
     }
 
-    const getSum = () => {
-      console.log('getSum')
-      return good + neutral + bad
-    }
-
-    const getAverage = () => {
-      console.log('getAverage')
-      return (good*1 + neutral*0 + bad*(-1)) / 3
-    }
-
-    const getPositive = () => {
-      console.log('getPositive')
-      return (good / getSum()) *100 + ' %'
-    }
-
 
   return (
     <div>
@@ -55,9 +62,7 @@ const App = () => {
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
       <Statistics good={good} neutral={neutral} bad={bad} />
-      <p>all {getSum()} </p>
-      <p>average {getAverage()} </p>
-      <p>positive {getPositive()} </p>
+
     </div>
   )
 }
