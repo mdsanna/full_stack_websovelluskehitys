b1c0af389abe1c45b008bc5424d6d80c66c18ada
Sanna Martikainen <martikainen.sanna@gmail.com>
2026-05-05 21:45:05 +0300

    Exercise 2.6

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -6,19 +6,41 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked: ', event.target)
+    const personObject = {
+      name: newName,
+      //id: String(persons.lenght+1),
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}  
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => 
+        <li key={person.name}>
+          {person.name}
+        </li> )}
     </div>
   )
 
