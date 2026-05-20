6798e7ae92217dabc889de1acb6ff19782f5f380
Sanna Martikainen <martikainen.sanna@gmail.com>
2026-05-20 19:38:32 +0300

    Exercise 2.12

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -34,7 +34,13 @@ const App = () => {
         number: newNumber
        }
       
-      setPersons(persons.concat(personObject))
+      axios
+       .post('http://localhost:3001/persons', personObject)
+       .then(response => {
+        setPersons(persons.concat(response.data))
+       })
+
+      
     }
     setNewName('')
     setNewNumber('')
