f14a2b61aeb7eec87e694850cf79cadf88ebedc4
Sanna Martikainen <martikainen.sanna@gmail.com>
2026-05-23 12:33:45 +0300

    Exercises 3.3 and 3.4

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -38,7 +38,25 @@ let persons =
     `)
   })
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    app.get('/api/persons/:id', (request, response) => {
+      const id = request.params.id 
+      const person = persons.find(person => person.id === id)
+
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+
+    app.delete('/api/persons/:id', (request, response) => {
+      const id = request.params.id 
+      persons = persons.filter(person => person.id != id)
+     
+      response.status(204).end()
+    })
+
+    const PORT = 3001
+      app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
