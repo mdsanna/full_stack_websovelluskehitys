abf7e174826138017a0cc7a6916c891fc6239935
Sanna Martikainen <martikainen.sanna@gmail.com>
2026-05-24 01:27:50 +0300

    Exercise 3.15

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -17,29 +17,6 @@ morgan.token('req-content', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-content'))
 
-let persons =
-[
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": "1"
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": "2"
-    }, 
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": "3"
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": "4"
-    }
-  ]
   //getting all persons
   app.get('/api/persons', (request, response) => {
     Person.find({})
@@ -62,7 +39,15 @@ let persons =
   app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id)
     .then(person => {
-      response.json(person)
+      if (person) {
+        response.json(person)
+      } else {
+          response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
     })
 
   })
@@ -95,15 +80,17 @@ let persons =
 
   })
 
-  /**
+
     //deleting a specific person
     app.delete('/api/persons/:id', (request, response) => {
-      const id = request.params.id 
-      persons = persons.filter(person => person.id != id)
-     
-      response.status(204).end()
+      Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => {
+        console.log(error)
+      })
     })
-*/ 
 
     //listening requests
     const PORT = process.env.PORT
