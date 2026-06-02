c6c8469461f991b5fa1b5a36edc76c41c6843f61
Sanna Martikainen <martikainen.sanna@gmail.com>
2026-06-02 21:54:15 +0300

    Exercises 4.5 and 4.6

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -3,35 +3,18 @@ const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 const totalLikes = require('../utils/list_helper').totalLikes
 
-test('dummy returns one', () => {
-  const blogs = []
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
-})
-
-describe('total of likes', () => {
-  test('of empty bloglist', () => {
-    assert.strictEqual(totalLikes([]), 0)
-  })
-
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  test('when list has only one blog equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 5)
-  })
-
-  const blogs = [
+  const blogList = [
   {
     _id: "5a422a851b54a676234d17f7",
     title: "React patterns",
@@ -82,11 +65,76 @@ describe('total of likes', () => {
   }  
 ]
 
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  assert.strictEqual(result, 1)
+})
+
+describe('total of likes', () => {
+  test('of empty bloglist', () => {
+    assert.strictEqual(totalLikes([]), 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
   test('of a bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(blogs)
+    const result = listHelper.totalLikes(blogList)
     assert.strictEqual(result, 36)
+  })
+})
+
+  describe('favourite blog', () => {
+  test('of empty bloglist', () => {
+    assert.deepStrictEqual(listHelper.favoriteBlog([]), {})
+  })
+
+  test('when list has only one blog it should be favourite', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, listWithOneBlog[0])
+  })
+
+  test('favorite from bigger list is returned', () => {
+    const result = listHelper.favoriteBlog(blogList)
+    const blog =   {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+    assert.deepStrictEqual(result, blog)
   })
 
 })
 
+  describe('most blogs', () => {
+  test('of empty bloglist', () => {
+    assert.deepStrictEqual(listHelper.mostBlogs([]), '')
+  })
+
+  test('when list has only one blog its author should have most blogs', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    const temp = {
+      author: "Edsger W. Dijkstra",
+      blogs: 1
+    }
+    assert.deepStrictEqual(result, temp)
+  })
+
+  test('author with most blogs should be returned', () => {
+    const result = listHelper.mostBlogs(blogList)
+    const temp = {
+      author: "Robert C. Martin",
+      blogs: 3
+    }
+    assert.deepStrictEqual(result, temp)
+  })
+
+})
 
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -9,6 +9,37 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const temp = blogs.reduce((prev, curr) => {
+      return (prev.likes > curr.likes ? prev : curr)
+    }, {})
+    return temp
+}
+
+
+const mostBlogs = (blogs) => {
+
+  if (blogs.length === 0) {
+    return ''
+  } 
+
+  const authors = blogs.reduce((sum, blog) => {
+    if (sum[blog.author]) {
+      sum[blog.author] += 1 
+    } else {
+      sum[blog.author] = 1   
+    }    
+    return sum
+  }, {})
+
+  const sorted = Object.entries(authors)
+    .map(([author, count]) => ({ author, blogs: count }))
+    .sort((a, b) => b.blogs - a.blogs)
+
+    return sorted[0]
+
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
