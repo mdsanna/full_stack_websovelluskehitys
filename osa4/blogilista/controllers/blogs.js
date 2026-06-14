c274a2afa74c0031ebaff1004ca620b0424769fd
Sanna Martikainen <martikainen.sanna@gmail.com>
2026-06-14 13:13:37 +0300

    Exercise 4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,8 +1,17 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const logger = require('../utils/logger')
 
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -25,8 +34,12 @@ blogsRouter.post('/', async (request, response) => {
 
   const { title, author, url, likes } = request.body
 
-  const users = await User.find({})
-  const user = users[0]
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   console.log('user: ', user)
 
   const blog = new Blog({
