3e4c2d3acff0d8eeca8ea4e8153f1a1f4d55521d
Sanna Martikainen <martikainen.sanna@gmail.com>
2026-06-16 22:39:04 +0300

    Exercise 5.1

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +17,42 @@ const App = () => {
     )  
   }, [])
 
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+
+     try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      console.log('wrong credentials')
+      setTimeout(() => {
+      }, 5000)
+    }   
+    
+  }
+
+if (user === null ){
+ return (
+ <div>
+    <h2>Log in to application</h2>
+    <LoginForm 
+      handleLogin = {handleLogin}
+      username = {username}
+      password = {password}
+      setUsername = {setUsername}
+      setPassword = {setPassword}
+      />
+ </div>
+ )
+}
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
